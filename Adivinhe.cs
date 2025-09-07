ae7657e32c18b78f1a82f35fea76ee342337d829
Wanblack17 <leandrochavesxbox@gmail.com>
2025-09-07 17:38:18 -0300

    Primeiro Commit: Jogo da adivinhação em C#

---
+++ Adivinhe.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Diagnostics.Eventing.Reader;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int numero = random.Next(1, 101);
+            int tentativa = -1;
+            int contador = 1;
+            for (int i = 1; i < 11; i++)
+            {
+                Console.WriteLine("Digite um numero: ");
+                tentativa = Convert.ToInt32(Console.ReadLine());
+                while (tentativa < 0 || tentativa > 100)
+                {
+                    Console.WriteLine("Numero Invalido! o valor deve ser entre 0 e 100!");
+                    tentativa = Convert.ToInt32(Console.ReadLine());
+                }
+                if (tentativa == numero)
+                {
+                    Console.WriteLine($"Parabens, você acertou na {contador}° tentativa!")
+                            ; break;
+                }
+                else if (tentativa < numero)
+                {
+                    Console.WriteLine("Chute mais alto!");
+                    
+                }
+                else if (tentativa > numero)
+                {
+                    Console.WriteLine("Chute mais baixo!:");
+                    
+
+                }
+                contador++;
+
+            }
+            Console.WriteLine($"O numero correto era {numero}");
+        }
+    }
+}
