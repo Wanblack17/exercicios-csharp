86c90f935ecf69beef0376c393ea84715cd43183
Wanblack17 <leandrochavesxbox@gmail.com>
2025-10-24 22:30:52 -0300

    Adicionando Programa que Converte Dólar

---
+++ Conversão-Dolar/Conversão-Dolar/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conversão_Dolar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double dolar = 0.0;
+            double real = 0.0;
+            Console.WriteLine("Qual a cotação atual do Dólar?");
+            dolar = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Quantos Reais você vai converter");
+            real = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"O valor convertido fica {Math.Round(Converter(real, dolar),2)} Dólares");
+        }
+    
+      static double Converter(double x, double y)
+        {
+            return x / y;
+        }
+    } }
