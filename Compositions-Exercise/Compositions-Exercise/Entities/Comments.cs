67368c8dc52b1622c0d396775a83fbcef8bcbad3
Wanblack17 <leandrochavesxbox@gmail.com>
2025-10-24 22:47:42 -0300

    Compositions Exercise

---
+++ Compositions-Exercise/Compositions-Exercise/Entities/Comments.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compositions_Exercise.Entities
+{
+    public class Comments
+    {
+        public string Text { get; set; }
+        public Comments() { }
+
+        public Comments(string text)
+        {
+            Text = text;
+        }
+    }
+}
---
+++ Compositions-Exercise/Compositions-Exercise/Program.cs
@@ -0,0 +1,17 @@
+﻿using Compositions_Exercise.Entities;
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        Comments c1 = new Comments("Have a nice trip!");
+        Comments c2 = new Comments("OW!! That's awesome");
+        Post p1 = new Post(DateTime.Parse("21/06/18 13:05:44"), "Travelling to new Zealand", "I'm going to visit this amazing country!", 12 );
+
+        p1.AddComment(c1);
+        p1.AddComment(c2);
+
+        Console.WriteLine(p1);
+    }
+}
