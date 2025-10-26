e2d5a1ab4039d10ff5cea5c96cdd7fa3b6f04fc9
Wanblack17 <leandrochavesxbox@gmail.com>
2025-10-26 16:05:40 -0300

    Simple Exercise with Exceptions

---
+++ Exceptions/Exceptions/Entities/Account.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+using Exceptions.Exceps;
+
+namespace Exceptions.Entities
+{
+    public class Account
+    {
+        
+
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance  { get; set; }
+        
+        public double WithDrawLimit { get; set; }
+
+
+        public Account()
+        {
+        }
+
+        public Account(int number, string holder, double balance, double withDrawLimit)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+            WithDrawLimit = withDrawLimit;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+        public void Withdraw (double amount)
+        {
+            if (amount > Balance)
+            {
+                throw new DomainException("Insufficient balance!");
+            }
+            else if (amount > WithDrawLimit)
+            {
+                throw new DomainException("The amount exceeds withdraw limit!");
+            }
+            Balance -= amount;
+        }
+
+
+
+    }
+}
---
+++ Exceptions/Exceptions/Program.cs
@@ -0,0 +1,33 @@
+﻿using Exceptions.Entities;
+using System;
+using System.Globalization;
+using Exceptions.Exceps;
+public class HelloWorld 
+{ 
+    public static void Main(string[] args) 
+    { 
+        Console.WriteLine("Enter Account Data: "); 
+        try 
+        { 
+            Console.Write("Number: "); 
+            int number = int.Parse(Console.ReadLine()); 
+            Console.Write("Holder: "); 
+            string holder = Console.ReadLine(); 
+            Console.Write("Initial Balance:"); 
+            double initial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Withdraw Limit: "); 
+            double withdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture); 
+            Account account = new Account(number, holder, initial, withdrawLimit); 
+            Console.Write("Enter amount fot withdraw: "); 
+            double withdraw = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture); 
+            account.Withdraw(withdraw); 
+            Console.WriteLine($"New Balance: {account.Balance.ToString("F2", CultureInfo.InvariantCulture)}"); } 
+        catch (DomainException e)
+        { 
+            Console.WriteLine($"ERROR: {e.Message}"); } 
+        catch (FormatException) 
+        { 
+            Console.WriteLine("ERROR: This field accepts numbers only "); 
+        }
+    }
+}
